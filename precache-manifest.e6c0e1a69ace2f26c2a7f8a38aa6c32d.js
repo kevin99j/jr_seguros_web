de100df9dc974679ba9e4732834a37f2b20010b1
root <root@DESKTOP-BRU44P3.localdomain>
2020-10-03 01:04:27 -0500

    Updates

---
+++ precache-manifest.e6c0e1a69ace2f26c2a7f8a38aa6c32d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae540cc7ad862480a62169a1d9920b92",
+    "url": "/jr_seguros_web/index.html"
+  },
+  {
+    "revision": "77c2ba97cf4fe2370128",
+    "url": "/jr_seguros_web/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6a08b51ed0b27b47ed5b",
+    "url": "/jr_seguros_web/static/css/main.bc98d9fe.chunk.css"
+  },
+  {
+    "revision": "77c2ba97cf4fe2370128",
+    "url": "/jr_seguros_web/static/js/2.b9525ba7.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/jr_seguros_web/static/js/2.b9525ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a08b51ed0b27b47ed5b",
+    "url": "/jr_seguros_web/static/js/main.1084b516.chunk.js"
+  },
+  {
+    "revision": "10b8ce9540afb3b25653",
+    "url": "/jr_seguros_web/static/js/runtime-main.2322c3b9.js"
+  },
+  {
+    "revision": "244c4ea1498fa428f5ee4b3a399b226e",
+    "url": "/jr_seguros_web/static/media/Flaticon.244c4ea1.svg"
+  },
+  {
+    "revision": "3013a1c6d856e554b257ed98d2aad35d",
+    "url": "/jr_seguros_web/static/media/Flaticon.3013a1c6.eot"
+  },
+  {
+    "revision": "83077578b4005fb5f965bd943857107f",
+    "url": "/jr_seguros_web/static/media/Flaticon.83077578.woff"
+  },
+  {
+    "revision": "ad6c4e4456bb650c6152855556b22b08",
+    "url": "/jr_seguros_web/static/media/Flaticon.ad6c4e44.ttf"
+  },
+  {
+    "revision": "37173202ba012d515cc7018bbe81fd6d",
+    "url": "/jr_seguros_web/static/media/bg_2.37173202.jpg"
+  },
+  {
+    "revision": "60bc6a3adfa4d39038c42a348591d5a9",
+    "url": "/jr_seguros_web/static/media/dept-1.60bc6a3a.jpg"
+  },
+  {
+    "revision": "3a9fb5a1c948ae38fe7c6e4cc139e11b",
+    "url": "/jr_seguros_web/static/media/principalLogo.3a9fb5a1.png"
+  }
+]);
